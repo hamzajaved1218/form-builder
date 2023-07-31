b14420a8645275b6606890cc249da71b56d6c63f
hamzajaved111 <hamza.javed@commerciotech.com>
2023-07-31 23:55:14 +0500

    21

--- src/FormDesigner.js
+++ src/FormDesigner.js
@@ -135,7 +135,7 @@ const FormDesigner = () => {
               <input
                 type="text"
                 id="form_name"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500 md:w-80"
+                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:text-white  dark:focus:border-blue-500 dark:focus:ring-blue-500 md:w-80"
                 placeholder="John"
                 required
               />
